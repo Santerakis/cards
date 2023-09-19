fcce1e5f3b29c5800bd24d42ef86b055ec575105
Sergey <santerakis@gmail.com>
2023-09-19 11:56:58 +0300

    modal_create-deck

---
+++ src/components/ui/modal/create-deck/create-deck.stories.tsx
@@ -0,0 +1,35 @@
+import { Meta, StoryObj } from '@storybook/react'
+
+import { Button, CreateDeck } from '@/components'
+
+const meta = {
+  title: 'Modals/Create Deck',
+  component: CreateDeck,
+  tags: ['autodocs'],
+  decorators: [
+    Story => (
+      <div style={{ margin: '3em', display: 'flex', justifyContent: 'center' }}>
+        <Story />
+      </div>
+    ),
+  ],
+  args: {
+    trigger: <Button variant={'primary'}>Create Deck</Button>,
+  },
+} satisfies Meta<typeof CreateDeck>
+
+export default meta
+type Story = StoryObj<typeof meta>
+export const Default: Story = {
+  render: args => {
+    return (
+      <CreateDeck
+        trigger={args.trigger}
+        onSubmit={data => {
+          // eslint-disable-next-line no-console
+          console.log(data)
+        }}
+      />
+    )
+  },
+}
