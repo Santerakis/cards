ae3ceb9f83aaa02d0f418c54635d03950c867fed
Sergey <santerakis@gmail.com>
2023-09-20 13:40:25 +0300

    textfield

---
+++ src/components/ui/textfield/text-field.stories.tsx
@@ -0,0 +1,27 @@
+import type { Meta, StoryObj } from '@storybook/react'
+
+import { TextField } from './'
+
+const meta = {
+  title: 'Components/TextField',
+  component: TextField,
+  tags: ['autodocs'],
+  decorators: [
+    Story => (
+      <div style={{ margin: '3em', display: 'flex', justifyContent: 'center' }}>
+        <Story />
+      </div>
+    ),
+  ],
+  argTypes: {},
+} satisfies Meta<typeof TextField>
+
+export default meta
+type Story = StoryObj<typeof meta>
+export const Default: Story = {
+  args: { title: 'test', inputType: 'text' },
+}
+
+export const Disabled: Story = {
+  args: { disabled: true, title: 'test', inputType: 'text' },
+}
---
+++ src/components/ui/textfield/text-field.tsx
@@ -0,0 +1,75 @@
+import { ComponentProps, forwardRef, useState } from 'react'
+
+import { clsx } from 'clsx'
+
+import s from './text-field.module.scss'
+
+import { SearchIcon, ShowIcon } from '@/assets'
+import HideIcon from '@/assets/icons/HideIcon.tsx'
+import { Typography } from '@/components'
+
+export type InputProps = {
+  containerProps?: ComponentProps<'div'>
+  title?: string
+  inputType: 'text' | 'password' | 'search'
+  error?: string
+  className?: string
+} & Omit<React.ComponentPropsWithoutRef<'input'>, 'type'>
+
+export const TextField = forwardRef<HTMLInputElement, InputProps>(
+  ({ className, title, inputType, disabled, error, containerProps, ...rest }, ref) => {
+    const [showPassword, setShowPassword] = useState(false)
+    const color = disabled ? 'var(--color-dark-300)' : 'var(--color-light-100)'
+    const searchIconColor = disabled ? 'var(--color-dark-300)' : 'var(--color-dark-100)'
+    const cNames = {
+      input: clsx(
+        s.input,
+        inputType === 'password' && s.password,
+        inputType === 'search' && s.search,
+        error && s.inputError
+      ),
+      container: clsx(s.inputContainer),
+      root: clsx(s.root, className, containerProps?.className),
+      search: clsx(s.searchIcon),
+      error: clsx(s.error),
+      label: clsx(s.label),
+    }
+    const type = showPassword && inputType === 'password' ? 'text' : inputType
+
+    const showHidePassword = () => {
+      if (!disabled) {
+        setShowPassword(!showPassword)
+      }
+    }
+
+    const rightIcons = inputType === 'password' && (
+      <div className={s.rightIcon} onClick={showHidePassword}>
+        {showPassword && !disabled ? <HideIcon color={color} /> : <ShowIcon color={color} />}
+      </div>
+    )
+
+    const leftIcon = inputType === 'search' && (
+      <div className={cNames.search}>{<SearchIcon color={searchIconColor} />}</div>
+    )
+
+    const errorMessage = error && (
+      <Typography variant="caption" color="error" unselectable="on">
+        {error}
+      </Typography>
+    )
+
+    return (
+      <div className={cNames.root}>
+        <Typography className={cNames.label} variant="body2" color="secondary" unselectable="on">
+          {title}
+          <div className={cNames.error}>{errorMessage}</div>
+        </Typography>
+        <div className={cNames.container}>
+          <input ref={ref} disabled={disabled} className={cNames.input} type={type} {...rest} />
+          {rightIcons}
+          {leftIcon}
+        </div>
+      </div>
+    )
+  }
+)
