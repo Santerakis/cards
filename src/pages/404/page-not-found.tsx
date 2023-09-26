bee3086a64843a667ea2bd49e39ef0b68bbb5f12
Sergey <santerakis@gmail.com>
2023-09-26 23:40:28 +0300

    page-not-found

---
+++ src/pages/404/page-not-found.tsx
@@ -0,0 +1,35 @@
+import { FC } from 'react'
+
+import { clsx } from 'clsx'
+import { useNavigate } from 'react-router-dom'
+//todo uncomment
+//import { useNavigate } from 'react-router-dom'
+
+import s from './page-not-found.module.scss'
+
+import { Page404 } from '@/assets'
+import { Button, Typography } from '@/components'
+
+export const PageNotFound: FC = () => {
+  const navigate = useNavigate()
+  const classNames = {
+    wrapper: clsx(s.wrapper),
+    container: clsx(s.container),
+    message: clsx(s.message),
+    btn: clsx(s.btn),
+  }
+
+  return (
+    <div className={classNames.wrapper}>
+      <Page404 />
+      <div className={classNames.container}>
+        <Typography variant={'body1'} className={classNames.message}>
+          Sorry! Page not found!
+        </Typography>
+        <Button as={'a'} href={'/'} className={classNames.btn} onClick={() => navigate('/')}>
+          Back to home page
+        </Button>
+      </div>
+    </div>
+  )
+}
