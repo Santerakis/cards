0a15ae94fdd6661024c64719ab7f7389f0cf9378
Sergey <santerakis@gmail.com>
2023-10-14 22:27:59 +0300

    step

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,68 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+import { Layout } from '@/components'
+import { Cards, CheckEmailPage, Decks, PageNotFound, Profile, SignIn, SignUp } from '@/pages'
+import { ForgotPasswordPage } from '@/pages/forgot-password'
+import { Learn } from '@/pages/learn'
+import { UpdatePasswordPage } from '@/pages/update-password'
+import { ProtectedRoutes } from '@/protected-routes.tsx'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout />,
+    children: [
+      {
+        element: <ProtectedRoutes />,
+        children: [
+          {
+            index: true,
+            element: <Decks />,
+          },
+          {
+            path: 'cards/:id',
+            element: <Cards />,
+          },
+          {
+            path: 'profile',
+            element: <Profile />,
+          },
+          {
+            path: 'learn/:id',
+            element: <Learn />,
+          },
+        ],
+      },
+      {
+        path: 'sign-in',
+        element: <SignIn />,
+      },
+      {
+        path: '/sign-up',
+        element: <SignUp />,
+      },
+
+      {
+        path: 'forgot_password',
+        element: <ForgotPasswordPage />,
+      },
+      {
+        path: 'reset-password/:token',
+        element: <UpdatePasswordPage />,
+      },
+
+      {
+        path: 'check-email',
+        element: <CheckEmailPage />,
+      },
+      {
+        path: '*',
+        element: <PageNotFound />,
+      },
+    ],
+  },
+])
+
 export function App() {
-  return <div>Hello</div>
+  return <RouterProvider router={router} />
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,31 @@
-import './styles/index.scss'
 import '@fontsource/roboto/400.css'
 import '@fontsource/roboto/700.css'
-import { StrictMode } from 'react'
 
 import { createRoot } from 'react-dom/client'
+import { Provider } from 'react-redux'
+import { ToastContainer } from 'react-toastify'
 
 import { App } from './App.tsx'
 
+import { store } from '@/app/store.ts'
+import 'react-toastify/dist/ReactToastify.min.css'
+
+import './styles/index.scss'
+
 createRoot(document.getElementById('root') as HTMLElement).render(
-  <StrictMode>
+  <Provider store={store}>
     <App />
-  </StrictMode>
+    <ToastContainer
+      position="bottom-left"
+      autoClose={5000}
+      hideProgressBar={false}
+      newestOnTop={false}
+      closeOnClick
+      rtl={false}
+      pauseOnFocusLoss
+      draggable
+      pauseOnHover
+      theme="dark"
+    />
+  </Provider>
 )
